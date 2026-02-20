15f79a0c56a1b36688c955cd771fad80d76b5e54
João Henrique da silva <7joaohenrique5557@gmail.com>
2026-02-20 20:21:53 -0300

    projeto_estavel

--- app/src/main/java/com/example/calculadora_em_java/MainActivity.java
+++ app/src/main/java/com/example/calculadora_em_java/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.calculadora_em_java;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Double num1, num2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,71 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
+
+        EditText value1 = (EditText) findViewById(R.id.input1);
+        EditText value2 = (EditText) findViewById(R.id.input2);
+
+        Button btn_somar = (Button) findViewById(R.id.somar);
+        Button btn_subtrair = (Button) findViewById(R.id.subtrair);
+        Button btn_multiplicar = (Button) findViewById(R.id.multiplicar);
+        Button btn_dividir = (Button) findViewById(R.id.dividir);
+
+        TextView saida = (TextView) findViewById(R.id.resultado);
+
+
+
+        btn_somar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        num1 = Double.parseDouble(value1.getText().toString());
+                        num2 = Double.parseDouble(value2.getText().toString());
+                        Double soma = num1 + num2;
+
+                        saida.setText(soma.toString());
+                    }
+                }
+        );
+
+        btn_subtrair.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        num1 = Double.parseDouble(value1.getText().toString());
+                        num2 = Double.parseDouble(value2.getText().toString());
+                        Double diferenca = num1 - num2;
+
+                        saida.setText(diferenca.toString());
+                    }
+                }
+        );
+
+        btn_multiplicar.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        num1 = Double.parseDouble(value1.getText().toString());
+                        num2 = Double.parseDouble(value2.getText().toString());
+                        Double mul = num1 * num2;
+
+                        saida.setText(mul.toString());
+                    }
+                }
+        );
+
+        btn_dividir.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        num1 = Double.parseDouble(value1.getText().toString());
+                        num2 = Double.parseDouble(value2.getText().toString());
+                        Double divi = num1 / num2;
+
+                        saida.setText(divi.toString());
+                    }
+                }
+        );
     }
 }
